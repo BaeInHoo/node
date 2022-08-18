4f3db96f83aa224946f5fbfd856e82a553557285
BaeInHoo <qodlsgh0210@naver.com>
2022-08-19 00:29:28 +0900

    실시간 gif 채팅방 만들기

--- git-chat/app.js
+++ git-chat/app.js
@@ -7,8 +7,9 @@ const nunjucks = require('nunjucks');
 const dotenv = require('dotenv');
 
 dotenv.config();
-const webSocket = require('./socket')
+const webSocket = require('./socket');
 const indexRouter = require('./routes');
+const connect = require('./schemas');
 
 const app = express();
 app.set('port', process.env.PORT || 8005);
@@ -17,6 +18,7 @@ nunjucks.configure('views', {
   express: app,
   watch: true,
 });
+connect();
 
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
