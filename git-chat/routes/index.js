09ff22c00f4593200aa8338baf7836ec38d9ca7c
BaeInHoo <qodlsgh0210@naver.com>
2022-08-25 17:21:33 +0900

    미들웨어 소켓 연결하기

--- git-chat/routes/index.js
+++ git-chat/routes/index.js
@@ -40,20 +40,25 @@ router.get('/room/:id', async (req, res, next) => {
   try {
     const room = await Room.findOne({ _id: req.params.id });
     const io = req.app.get('io');
+
     if (!room) {
       return res.redirect('/?error=존재하지 않는 방입니다.');
     }
+
     if (room.password && room.password !== req.query.password) {
       return res.redirect('/?error=비밀번호가 틀렸습니다.');
     }
+
     const { rooms } = io.of('/chat').adapter;
     if (rooms && rooms[req.params.id] && room.max <= rooms[req.params.id].length) {
       return res.redirect('/?error=허용 인원을 초과했습니다.');
     }
+
+    const chats = await Chat.find({ room: room._id }).sort('createAt');
     return res.render('chat', {
       room,
       title: room.title,
-      chat: [],
+      chats,
       user: req.session.color,
     });
   } catch (err) {
@@ -70,6 +75,21 @@ router.delete('/room/:id', async (req, res, next) => {
     setTimeout(() => {
       req.app.get('io').of('/room').emit('removeRoom', req.params.id);
     }, 2000);
+  } catch (err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+router.post('/room/:id/chat', async (req, res, next) => {
+  try {
+    const chat = await Chat.create({
+      room: req.params.id,
+      user: req.session.color,
+      chat: req.body.chat,
+    });
+    req.app.get('io').of('/chat').to(req.params.id).emit('chat', chat);
+    res.send('ok');
   } catch (err) {
     console.error(err);
     next(err);
