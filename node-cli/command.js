669d8308c0ea119b92e3211f62e409e9b12655c2
BaeInHoo <qodlsgh0210@naver.com>
2022-09-11 13:37:31 +0900

    commander upgrade

--- node-cli/command.js
+++ node-cli/command.js
@@ -1,5 +1,82 @@
-#!/usr/bin/rnv node
-const  { program } =require('commander');
+#!/usr/bin/env node
+const  { program } = require('commander');
+const fs = require('fs');
+const path = require('path');
+
+const htmlTemplate = `
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta chart="utf-8: />
+    <title>Template</title>
+  </head>
+  <body>
+    <h1>Hello</h1>
+    <p>CLI</p>
+  </body>
+</html>
+`;
+
+const routerTemplate = `
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+  try {
+    res.send('ok');
+  } catch(err) {
+    console.error(err);
+    next(err);
+  }
+});
+
+module.exports = router;
+`;
+
+const exist = (dir) => {
+  try {
+    fs.accessSync(dir, fs.constants.F_OK || fs.constants.W_OK || fs.constants.R_OK);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
+
+const mkdirp = (dir) => {
+  const dirname = path
+    .relative('.', path.normalize(dir))
+    .split(path.sep)
+    .filter(p => !!p);
+  dirname.forEach((d, idx) => {
+    const pathBuilder = dirname.slice(0, idx + 1).join(path.sep);
+    if (!exist(pathBuilder)) {
+      fs.mkdirSync(pathBuilder);
+    }
+  });
+};
+
+const makeTemplate = (type, name, directory) => {
+  mkdirp(directory);
+  if (type === 'html') {
+    const pathToFile = path.join(directory, `${name}.html`);
+    if(exist(pathToFile)) {
+      console.error('이미 해당 파일이 존재합니다');
+    } else {
+      fs.writeFileSync(pathToFile, htmlTemplate);
+      console.log(pathToFile, '생성 없음');
+    }
+  } else if (type === 'express-router') {
+    const pathToFile = path.join(directory, `${name}.js`);
+    if (exist(pathToFile)) {
+      console.error('이미 해당 파일이 존재합니다');
+    } else {
+      fs.writeFileSync(pathToFile, routerTemplate);
+      console.log(pathToFile, '생성 완료');
+    }
+  } else {
+    console.error('html 또는 express-router 둘 중 하나를 입력하세요.');
+  }
+};
 
 program
   .version('0.0.1', '-v, -version')
